8e1c6e4cd728835c091ac0732c1e4ba58c80c3d0
sayarozi <achmad_23510017@stimata.ac.id>
2024-10-16 21:41:27 +0700

    aplikasi sederhana

---
+++ AppSederhana/app/src/main/java/com/example/appsederhana/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.appsederhana
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun submit(view: View){
+        val namaET = findViewById<EditText>(R.id.et_nama)
+        val nama = namaET.text.toString()
+        val toast = Toast.makeText(this, nama, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+}
