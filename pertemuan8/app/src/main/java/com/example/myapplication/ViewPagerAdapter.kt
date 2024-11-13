3896ae30f1192930303815afdb522030c1d132a7
sayarozi <achmad_23510017@stimata.ac.id>
2024-11-13 21:35:30 +0700

    pertemuan 8

---
+++ pertemuan8/app/src/main/java/com/example/myapplication/ViewPagerAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> FriendsFragment()
+            1 -> GithubFragment()
+            else -> ProfileFragment()
+        }
+    }
+}
